e9a2e41366e1a0d1a23bb74d40205f4f272bd11d
Tomás Goyret <tomas.goyret@gmail.com>
2022-09-01 15:56:18 -0300

    takeWhile

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
-import { first, fromEvent, map, tap } from "rxjs";
+import { fromEvent, map, of, takeWhile } from "rxjs";
 
 
-const click$ = fromEvent<MouseEvent> ( document, 'click');
+
+const click$ = fromEvent<MouseEvent> ( document, 'click')
 
 click$.pipe(
-    //first() // Es lo mismo que hacer take(1)
-    //Si quiero el primer evento en donde el click tenga el primer clientY mayor a 150
-    // tap<MouseEvent>(console.log),
-    //first<MouseEvent>( event => event.clientY >= 150)
-    //extra tip
-    // map( event => ({
-    //     clientY: event.clientY,
-    //     clientX: event.clientX,
-    // })),
-    map( ({clientY,clientX}) => ({clientY,clientX})),
-    first( event => event.clientY >= 150)
-).subscribe({
+    map( ({x , y}) => ({ x , y })),
+    takeWhile(({ y }) => y <= 150, true)
+)
+.subscribe({
     next: val => console.log('next', val),
     complete: () => console.log('complete')
+    
     
 })
