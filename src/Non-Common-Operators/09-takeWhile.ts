f88c28ca74c1d2aa0349291deaaff1c3a95ac4eb
Tomás Goyret <tomas.goyret@gmail.com>
2022-09-01 15:57:34 -0300

    takeWhile

---
+++ src/Non-Common-Operators/09-takeWhile.ts
@@ -0,0 +1,16 @@
+import { fromEvent, map, of, takeWhile } from "rxjs";
+
+
+
+const click$ = fromEvent<MouseEvent> ( document, 'click')
+
+click$.pipe(
+    map( ({x , y}) => ({ x , y })),
+    takeWhile(({ y }) => y <= 150, true)
+)
+.subscribe({
+    next: val => console.log('next', val),
+    complete: () => console.log('complete')
+    
+    
+})
