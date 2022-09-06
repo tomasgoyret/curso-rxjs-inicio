237e4d2a7b32d6dc387ecf07e3e99bd9da66ee58
Tomás Goyret <tomas.goyret@gmail.com>
2022-09-06 11:28:46 -0300

    mergeMap

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,29 @@
-import { debounceTime, fromEvent, map, mergeAll, Observable, pluck } from "rxjs"
-import { ajax } from "rxjs/ajax"
-import { GitHubUser, GitHubUsers } from "./interfaces/interfaces"
+import { fromEvent, interval, map, mergeMap, of, take, takeUntil } from "rxjs";
 
-const body = document.querySelector('body')
-const textInput = document.createElement('input')
-const orderList = document.createElement('ol')
-
-body.append( textInput, orderList)
-
-//Helpers
-
-const mostrarUsuarios = ( usuarios) => {
-    console.log(usuarios);
-
-    orderList.innerHTML='';
-
-    for (const usuario of usuarios) {
-        const li = document.createElement('li');
-        const img = document.createElement('img');
-        img.src = usuario.avatar_url
-        const anchor = document.createElement('a')
-        anchor.href = usuario.html_url
-        anchor.text = 'Ver página';
-        anchor.target = '_blank'
-
-        li.append( img )
-        li.append ( usuario.login + ' ')
-        li.append( anchor );
-
-        orderList.append(li)
-
-    }
-    
-}
-
-const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup')
-
-input$.pipe(
-    debounceTime<KeyboardEvent>(500), // recibe y devuelve keyboard event
-    pluck<KeyboardEvent>( 'target', 'value'), // recibe keyboard event pero te devuelve un unknow si no lo tipamos
-    map<unknown,Observable<GitHubUsers>>( texto => ajax.getJSON(`https://api.github.com/search/users?q=${texto}`)),
-    mergeAll<Observable<GitHubUsers>>(),
-    pluck<GitHubUsers>('items') //    pluck<GitHubUsers[]>('items')
-
-).subscribe( mostrarUsuarios )
+const letras$ = of('a','b', 'c');
 
 
+
+letras$.pipe(
+    mergeMap((letra) => interval(1000).pipe(
+        map( i => letra + i),
+        take(3)
+    ))
+)
+// .subscribe({
+//     next: val => console.log('next: ', val),
+//     complete: () => console.log('Complete'),
+// })
+
+
+const mouseDown$ = fromEvent( document, 'mousedown')
+const mouseUp$ = fromEvent( document, 'mouseup')
+
+const interval$ = interval();
+
+mouseDown$.pipe(
+    mergeMap(()=> interval$.pipe(
+        takeUntil( mouseUp$)
+    ))
+)
+.subscribe( console.log )
